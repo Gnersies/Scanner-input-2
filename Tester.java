5f295141bb7bb2346f5b0635b03fb6b6e5c8acfc
Gnersies <justin.morgan99@yahoo.com>
2016-09-13 11:46:40 -0600

    .

---
+++ Tester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Tester {
+	public static void main(String[] args) {
+		Scanner kbScanner = new Scanner(System.in);
+		System.out.print("What is your first name? ");
+		String a = kbScanner.next();
+		System.out.print("What is your last name? ");
+		String b = kbScanner.next();
+		System.out.print("Your full name is " + a +" "+ b);
+		
+	}
+}
+
+	
+
+
+
+
